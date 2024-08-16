2921af3a72619eb4155d7562fe4d13460a28a758
Wathsala-Rajapaksha <menuwathsala98@gmail.com>
2024-08-16 15:34:20 +0530

    Update files

--- client/src/dashboard/layout/Sidebar.jsx
+++ client/src/dashboard/layout/Sidebar.jsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link} from 'react-router-dom'
 
 const Sidebar = () => {
   return (
     <div className= 'w-[250px] h-screen fixed left-0 top-0 bg-white'>
         <div className = 'h- [70px] flex justify-center items-center' > 
             <Link>
-                <img src="" alt="" />
+                <img src="https://news-portal-mern.onrender.com/assets/logo-00ebaab6.png" alt="" />
             </Link>
       
       </div>

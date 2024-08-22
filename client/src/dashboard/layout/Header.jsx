d0ce5ba13d628eaa1ed64d411f7ccb82638764f8
Wathsala-Rajapaksha <menuwathsala98@gmail.com>
2024-08-22 12:33:42 +0530

    update files

--- client/src/dashboard/layout/Header.jsx
+++ client/src/dashboard/layout/Header.jsx
@@ -1,11 +1,23 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import profile from '../../assets/profile.png'
 
 const Header = () => {
   return (
-    <>
-      
-    </>
+    <div className='fixed p1-4 w-{calc(100vw-250px)} top-4 z-50'>
+      <div className='w-full rounded h-[70px] flex justify-between items-center p-4 bg-white'>
+         <input type= "text" placeholder='search' className='h-10 px-3 py-2 border border-gray-300 rounded-md focus:border-green-500 outline-0'/>
+        <div className='mr-4'>
+          <div className='flex gap-x-2'>
+           <div className='flex flex-col items-end justify-center'>
+             <span>Menu Wathsala</span>
+             <span>Admin</span>
+           </div> 
+           <img className='w-10 h-10 rounded-full' src={profile} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/dashboard/layout/MainLayout.jsx
+++ client/src/dashboard/layout/MainLayout.jsx
@@ -1,14 +1,25 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Sidebar from './Sidebar'
+import Header from './Header'
+
 import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
   return (
     
-    <div className='min-w-screen bg-slate-100'>
+    <div className='min-h-screen min-w-screen bg-slate-100'>
          <Sidebar/>
-         <Outlet/>
+         <div className='m1-[250px] w-[calc(100vw-250px)] min-h-[vh]'>
+          <Header />
+          <div className='p-4'>
+              <div className='pt-[85px]'>
+                   <Outlet/>
+              </div> 
+            
+          </div>
+         </div>
+        
     </div>
   )
 }
